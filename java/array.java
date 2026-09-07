f1a4712e5e540e69c39d0817c9e4b4dfa589d318
muskan-kumari-codes <muskanmalikyadav01@gmail.com>
2026-09-07 18:14:10 +0530

    updated code

--- java/array.java
+++ java/array.java
@@ -1,52 +1,52 @@
 public class array {
     public static void main(String[] args){
-        // int arr[]={1,2,91,3,4};
-        //int n= arr.length;
+        int arr[]={1,2,91,3,4};
+        int n= arr.length;
 
-
-        // int sum_of_elements = 0;
-        // int multiplication_of_elements = 1;
-        // for(int i =0; i<n;i++){
-        //     sum_of_elements+=arr[i];
-        //     multiplication_of_elements*=arr[i];
-        // }
-        // System.out.println("sum of elements in array : " + sum_of_elements);
-        // System.out.println("multiplication of elements in array : " + multiplication_of_elements);
+            // sum and multiplication of elements in array
+        int sum_of_elements = 0;
+        int multiplication_of_elements = 1;
+        for(int i =0; i<n;i++){
+            sum_of_elements+=arr[i];
+            multiplication_of_elements*=arr[i];
+        }
+        System.out.println("sum of elements in array : " + sum_of_elements);
+        System.out.println("multiplication of elements in array : " + multiplication_of_elements);
         
 
-        // // max element in array..
-        // int max = 0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]>=max){
-        //         max=arr[i];
-        //     }
-        // }
-        // System.out.println(max);
+        // max element in array..
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
 
 
         // minimum element in array..
-        // int mini = arr[0];
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]<mini){
-        //         mini=arr[i];
-        //     }
-        // }
-        // System.out.println(mini);
+        int mini = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]<mini){
+                mini=arr[i];
+            }
+        }
+        System.out.println(mini);
 
 
         // reverse array....
-    //    int start = 0;
-    //    int end = arr.length-1;
-    //    while(start<end){
-    //     int temp = arr[start];
-    //     arr[start] = arr[end];
-    //     arr[end] = temp;
-    //     start++;
-    //     end--;
-    //    }
-    //    for(int i=0; i<arr.length; i++){
-    //    System.out.print(arr[i] + " ");
-    //    }
+       int start = 0;
+       int end = arr.length-1;
+       while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+       }
+       for(int i=0; i<arr.length; i++){
+       System.out.print(arr[i] + " ");
+       }
     
-
+    }
 }
--- java/collectionFramework/level3.java
+++ java/collectionFramework/level3.java
@@ -5,90 +5,94 @@ import java.util.*;
 public class level3 {
     public static void main(String[] args) {
              //.....stack............
-        // Stack<Integer> stack = new Stack<>();
-        // stack.push(20);
-        // stack.push(9);
-        // stack.push(29);
-        // stack.push(60);
-        // stack.push(30);
-        // System.out.println(stack + " \n");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(20);
+        stack.push(9);
+        stack.push(29);
+        stack.push(60);
+        stack.push(30);
+        System.out.println(stack + " \n");
         
-        // System.out.println(stack.pop());
-        // System.out.println(stack + "\n");
+        System.out.println(stack.pop());
+        System.out.println(stack + "\n");
 
-        // System.out.println(stack.peek());
-        // System.out.println(stack + "\n");
+        System.out.println(stack.peek());
+        System.out.println(stack + "\n");
 
-        // String str = new String();
-        // str = "raam is a boy";
-        // System.out.println(str);
-        // String[] str2 = str.split(" ");
-        // Stack<String> stack2 = new Stack<>();
-        // for(String word : str2){
-        //    stack2.push(word);
-        // }
+            // reversing a string
 
-        // String rev = new String();
-        // while(stack2.size()>0){
-        //     rev = rev + " " + stack2.peek();
-        //     stack2.pop();
-        // }
-        //  System.out.println(rev);
+        String str = new String();
+        str = "raam is a boy";
+        System.out.println(str);
+        String[] str2 = str.split(" ");
+        Stack<String> stack2 = new Stack<>();
+        for(String word : str2){
+           stack2.push(word);
+        }
+        String rev = new String();
+        while(stack2.size()>0){
+            rev = rev + " " + stack2.peek();
+            stack2.pop();
+        }
+         System.out.println(rev);
 
           /// brackets  order....
-        // String str = "{[)]}";
-        // Stack<Character> stack = new Stack<>();
-        // for(char c : str.toCharArray()){
-        //     if(c =='(' || c == '{' || c=='['){
-        //         stack.push(c);
-        //     }else {
-        //         if(str.isEmpty()){
-        //             System.out.println("wrong order");
-        //             break;
-        //         }else{
-        //             char ch = stack.peek();
-        //             if((ch=='(' && c==')') || (ch=='{' && c=='}') || (ch=='[' && c==']')){
-        //                 stack.pop();
-        //             }else{
-        //                 System.out.println("wrong answer");
-        //                 break;
-        //             }
-        //         }
-        //     }
+        
+        String s = "{[)]}";
+        Stack<Character> stack3 = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c =='(' || c == '{' || c=='['){
+                stack3.push(c);
+            }else {
+                if(s.isEmpty()){
+                    System.out.println("wrong order");
+                    break;
+                }else{
+                    char ch = stack3.peek();
+                    if((ch=='(' && c==')') || (ch=='{' && c=='}') || (ch=='[' && c==']')){
+                        stack3.pop();
+                    }else{
+                        System.out.println("wrong answer");
+                        break;
+                    }
+                }
+            }
 
-        // }
-        //     if(stack.isEmpty()){
-        //         System.out.println("correct order");
-        //     }else{
-        //         System.out.println("wrong order");
-        //     }
-
-                // valid parentheses............
-        //String s = "{}()";
-
-
+        }
+            if(stack3.isEmpty()){
+                System.out.println("correct order");
+            }else{
+                System.out.println("wrong order");
+        }
 
 
                  //queue......
-        // Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        //     queue.offer(23);
-        //     queue.offer(30);
-        //     queue.offer(34);
-        //     queue.offer(50);
-        
-            // System.out.println(queue);
-            // System.out.println(queue.peek());
-            // System.out.println(queue.poll());
-            // System.out.println(queue);
-              
-                // k smallest elements..........
-
-            Queue<Integer> queue = new PriorityQueue<>();
+        Queue<Integer> queue = new PriorityQueue<>();
             queue.offer(23);
             queue.offer(30);
             queue.offer(34);
             queue.offer(50);
-            int k = 2;
+            queue.offer(5);
+            queue.offer(9);
+        
+            System.out.println(queue);
+            System.out.println(queue.peek());
+            System.out.println(queue.poll());
+            System.out.println(queue);
+
+                // k smallest elements
+            int k = 3;
+            for(int i=0; i<k; i++){
+                System.out.println(queue.poll());
+            }
+              
+                // k largest elements..........
+            Queue<Integer> queue2 = new PriorityQueue<>(Collections.reverseOrder());
+            queue2.offer(24);
+            queue2.offer(300);
+            queue2.offer(44);
+            queue2.offer(50);
+            k = 2;
             for(int i=0; i<k; i++){
                 System.out.println(queue.poll());
             }
