68f3346e7bfb585ea6c1e4c8d15c1e203bb1c9ca
muskan-kumari-codes <muskanmalikyadav01@gmail.com>
2026-09-19 18:28:01 +0530

    transpose of Matrix

---
+++ transpose.java
@@ -0,0 +1,22 @@
+package Practic.array;
+
+
+public class transpose {
+    static void Transpose(int[][] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        Transpose(arr);
+    }
+}
